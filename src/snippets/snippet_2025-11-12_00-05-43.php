eaf3ed96f9b592b07cf264618c4d88258a7c38c2
holyprof1 <tobbest17@gmail.com>
2025-11-12 00:05:43 +0100

    feat: add src\snippets\snippet_2025-11-12_00-05-43.php

---
+++ src/snippets/snippet_2025-11-12_00-05-43.php
@@ -0,0 +1,6 @@
+﻿<?php
+function str_slug(string $s): string {
+    $s = iconv('UTF-8', 'ASCII//TRANSLIT', $s);
+    $s = preg_replace('/[^a-zA-Z0-9]+/', '-', $s);
+    return strtolower(trim($s, '-'));
+}
