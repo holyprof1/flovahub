fce7e83f4a7d7de4637815ce4f775fbc0e4d4349
holyprof1 <tobbest17@gmail.com>
2025-11-11 21:59:48 +0100

    feat: add src\snippets\snippet_2025-11-11_21-59-48.php

---
+++ src/snippets/snippet_2025-11-11_21-59-48.php
@@ -0,0 +1,10 @@
+﻿<?php
+/**
+ * Utility: safe_json()
+ */
+function safe_json(array $data): string {
+    return json_encode(
+        $data,
+        JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_INVALID_UTF8_SUBSTITUTE
+    );
+}
